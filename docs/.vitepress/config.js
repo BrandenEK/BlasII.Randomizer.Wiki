ce349fcf78e3ced486b3b033ea3ac1a64c881ecb
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-21 12:30:35 -0400

    init: create vercel docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Blasphemous 2 Randomizer',
+  description: 'A wiki for everything related to the Blasphemous 2 randomizer',
+
+  themeConfig: {
+    // theme-level options
+  }
+}
